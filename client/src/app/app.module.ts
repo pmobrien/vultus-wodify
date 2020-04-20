5f903742b372eacf2f5238f9f12c4423576a089d
Patrick O'Brien <obrienp@outlook.com>
2020-04-19 23:20:10 -0500

    new combo box

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { DataTablesModule } from 'angular-datatables';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 import { AppService } from './app.service';
 import { AppComponent } from './app.component';
@@ -13,12 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    DataTablesModule,
+    FormsModule,
     HttpClientModule,
-    DataTablesModule
+    NgSelectModule
   ],
   providers: [
     AppService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
